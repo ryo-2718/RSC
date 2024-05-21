678a83e15e5d59d008240ef42020f9b9c7b3abe2
ryo-2718 <istudyyyyy@gmail.com>
2024-05-22 04:52:00 +0900

    Update setting.js

--- question/mock_test/setting/setting.js
+++ question/mock_test/setting/setting.js
@@ -1,19 +1,30 @@
- function toggleMockTestMode() {
-            const isMockTestMode = document.getElementById('mock-test-mode').checked;
-            const chapterCheckboxes = document.querySelectorAll('input[name="chapter"]');
-            const yearCheckboxes = document.querySelectorAll('input[name="year"]');
-            
-            chapterCheckboxes.forEach(checkbox => {
-                checkbox.checked = isMockTestMode;
-                checkbox.disabled = isMockTestMode;
-            });
-            
-            yearCheckboxes.forEach(checkbox => {
-                checkbox.checked = isMockTestMode;
-                checkbox.disabled = isMockTestMode;
-            });
-        }
-        
-        window.onload = () => {
-            toggleMockTestMode();
-        };
+const values = {};
+
+// 行を生成する関数
+function generateRows(years) {
+    return years.map(year => `
+        <tr>
+            <td>${year}</td>
+            <td><input type="checkbox" id="${year}_1" onchange="updateValue('${year}_1')"></td>
+            <td><input type="checkbox" id="${year}_2" onchange="updateValue('${year}_2')"></td>
+            <td><input type="checkbox" id="${year}_3" onchange="updateValue('${year}_3')"></td>
+            <td><input type="checkbox" id="${year}_4" onchange="updateValue('${year}_4')"></td>
+            <td><input type="checkbox" id="${year}_5" onchange="updateValue('${year}_5')"></td>
+        </tr>
+    `).join('');
+}
+
+function updateValue(id) {
+    const checkbox = document.getElementById(id);
+    values[id] = checkbox.checked;
+    console.log(values); // デバッグ用にコンソールに表示
+}
+
+// ページが読み込まれたときの初期設定
+window.onload = () => {
+    const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+    checkboxes.forEach(checkbox => {
+        values[checkbox.id] = checkbox.checked;
+    });
+    console.log(values); // デバッグ用にコンソールに表示
+};
