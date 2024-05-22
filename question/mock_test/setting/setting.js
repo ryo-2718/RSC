c71cf13a04b90ef0feb018252a59ada3897c0c6d
ryo-2718 <istudyyyyy@gmail.com>
2024-05-22 11:47:46 +0900

    Update setting.js

--- question/mock_test/setting/setting.js
+++ question/mock_test/setting/setting.js
@@ -82,6 +82,16 @@ function updateRelatedCheckboxes(id) {
     }
 }
 
+function toggleMockExamMode() {
+    const isMockExamMode = document.getElementById('mock-exam-toggle').checked;
+    const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+    checkboxes.forEach(checkbox => {
+        checkbox.checked = isMockExamMode;
+        checkbox.disabled = isMockExamMode; // トグルスイッチがオンの場合、チェックボックスを無効にする
+        updateValue(checkbox.id);
+    });
+}
+
 window.onload = () => {
     const years = ['H20_1', 'H20_2', 'H21', 'H22', 'H23', 'H24', 'H25', 'H26', 'H27', 'H28', 'H29', 'H30', 'R1', 'R2', 'R3', 'R4'];
     const tableBody = document.getElementById('table-body');
